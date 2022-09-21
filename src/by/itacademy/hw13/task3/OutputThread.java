c1d0c3054283d93ca61e4b2ae19f95d20378cf2d
Dasha Astakhova <dasha.kozhalo@gmail.com>
2022-09-21 19:56:36 +0300

    static field removed

--- src/by/itacademy/hw13/task3/OutputThread.java
+++ src/by/itacademy/hw13/task3/OutputThread.java
@@ -2,8 +2,6 @@ package by.itacademy.hw13.task3;
 
 public class OutputThread extends Thread {
 
-    private static int count = 1;
-
     @Override
     public void run() {
         Counter.print();
